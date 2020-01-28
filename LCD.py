80f5a681a1f5bbd3021ff9626a284d1b33a3fd37
Josh Bright <josh.bright@gmail.com>
2020-01-27 21:30:04 -0800

    #1 Changing enums out for variables

--- LCD.py
+++ LCD.py
@@ -1,60 +1,40 @@
 from I2CDevice import I2CDevice
 import time
-import enum
 
 
-class LCDCommands(enum.Enum):
-    CLEARDISPLAY = 0x01
-    RETURNHOME = 0x02
-    ENTRYMODESET = 0x04
-    DISPLAYCONTROL = 0x08
-    CURSORSHIFT = 0x10
-    FUNCTIONSET = 0x20
-    SETCGRAMADDR = 0x40
-    SETDDRAMADDR = 0x80
-
-
-class LCDDisplayEntryMode(enum.Enum):
-    ENTRYRIGHT = 0x00
-    ENTRYLEFT = 0x02
-    ENTRYSHIFTUP = 0x01
-    ENTRYSHIFTDOWN = 0x00
-
-
-class LCDOnOff(enum.Enum):
-    DISPLAYON = 0x04
-    DISPLAYOFF = 0x00
-    CURSORON = 0x02
-    CURSOROFF = 0x00
-    BLINKON = 0x01
-    BLINKOFF = 0x00
-
-
-class LCDCursor(enum.Enum):
-    DISPLAYMOVE = 0x08
-    CURSORMOVE = 0x00
-    MOVERIGHT = 0x04
-    MOVELEFT = 0x00
-
-
-class LCDMode(enum.Enum):
-    BITMODE_8 = 0x10
-    BITMODE_4 = 0x00
-    LINE_2 = 0x08
-    LINE_1 = 0x00
-    DOTS_5x10 = 0x04
-    DOTS_5x8 = 0x00
-
-
-class LCDBacklight(enum.Enum):
-    ON = 0x08
-    OFF = 0x00
-
-
-class Bits(enum.Enum):
-    ENABLE = 0b00000100
-    READ_WRITE = 0b00000010
-    SELECT = 0b00000001
+CLEARDISPLAY = 0x01
+RETURNHOME = 0x02
+ENTRYMODESET = 0x04
+DISPLAYCONTROL = 0x08
+CURSORSHIFT = 0x10
+FUNCTIONSET = 0x20
+SETCGRAMADDR = 0x40
+SETDDRAMADDR = 0x80
+ENTRYRIGHT = 0x00
+ENTRYLEFT = 0x02
+ENTRYSHIFTUP = 0x01
+ENTRYSHIFTDOWN = 0x00
+DISPLAYON = 0x04
+DISPLAYOFF = 0x00
+CURSORON = 0x02
+CURSOROFF = 0x00
+BLINKON = 0x01
+BLINKOFF = 0x00
+DISPLAYMOVE = 0x08
+CURSORMOVE = 0x00
+MOVERIGHT = 0x04
+MOVELEFT = 0x00
+BITMODE_8 = 0x10
+BITMODE_4 = 0x00
+LINE_2 = 0x08
+LINE_1 = 0x00
+DOTS_5x10 = 0x04
+DOTS_5x8 = 0x00
+BACKLIGHT_ON = 0x08
+OFF = 0x00
+ENABLE = 0b00000100
+READ_WRITE = 0b00000010
+SELECT = 0b00000001
 
 
 class LCD:
@@ -65,25 +45,20 @@ class LCD:
         self.lcd_write(0x03)
         self.lcd_write(0x02)
 
-        self.lcd_write(
-            LCDCommands.FUNCTIONSET
-            | LCDMode.LINE_2
-            | LCDMode.DOTS_5x8
-            | LCDMode.BITMODE_4
-        )
-        self.lcd_write(LCDCommands.DISPLAYCONTROL | LCDCommands.DISPLAYON)
-        self.lcd_write(LCDCommands.CLEARDISPLAY)
-        self.lcd_write(LCDCommands.ENTRYMODESET | LCDCommands.ENTRYLEFT)
+        self.lcd_write(FUNCTIONSET | LINE_2 | DOTS_5x8 | BITMODE_4)
+        self.lcd_write(DISPLAYCONTROL | DISPLAYON)
+        self.lcd_write(CLEARDISPLAY)
+        self.lcd_write(ENTRYMODESET | ENTRYLEFT)
         time.sleep(0.2)
 
     def lcd_strobe(self, data):
-        self.device.write_cmd(data | Bits.ENABLE | LCDBacklight.ON)
+        self.device.write_cmd(data | ENABLE | BACKLIGHT_ON)
         time.sleep(0.0005)
-        self.device.write_cmd(((data & Bits.ENABLE) | LCDBacklight.ON))
+        self.device.write_cmd(((data & ENABLE) | BACKLIGHT_ON))
         time.sleep(0.0001)
 
     def lcd_write_four_bits(self, data):
-        self.device.write_cmd(data | LCDBacklight.ON)
+        self.device.write_cmd(data | BACKLIGHT_ON)
         self.lcd_strobe(data)
 
     def lcd_write(self, cmd, mode=0):
@@ -101,8 +76,8 @@ class LCD:
             self.lcd_write(0xD4)
 
         for char in string:
-            self.lcd_write(ord(char), Bits.SELECT)
+            self.lcd_write(ord(char), SELECT)
 
     def lcd_clear(self):
-        self.lcd_write(LCDCommands.CLEARDISPLAY)
-        self.lcd_write(LCDCommands.RETURNHOME)
+        self.lcd_write(CLEARDISPLAY)
+        self.lcd_write(RETURNHOME)
