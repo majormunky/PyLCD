6d78d627852e00f082a3c24885b1472e3170fa31
Josh Bright <josh.bright@gmail.com>
2020-01-27 21:45:25 -0800

    Setup our main file to demo lcd

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import time
 from LCD import LCD
 
-
-lcd = LCD()
-lcd.lcd_display_string("Test", 1)
+try:
+    lcd = LCD()
+    while True:
+        lcd.lcd_display_string("Test", 1)
+        lcd.lcd_display_string("Line 2", 2)
+        time.sleep(2)
+        lcd.lcd_clear()
+        time.sleep(1)
+except KeyboardInterrupt:
+    lcd.lcd_clear()
